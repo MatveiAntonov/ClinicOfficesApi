8cfbc8058bb3d12223c998a237057dae37255529
ProsTuha <tuhalf2002@mail.ru>
2023-01-03 17:35:24 +0300

    Added CRUD interaction with database

--- Offices.WebApi/DatabaseSettings/OfficesDatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Offices.WebApi.DatabaseSettings;
-
-public class OfficesDatabaseSettings : IOfficesDatabaseSettings
-{
-    public string CoursesCollectionName { get; set; } = String.Empty;
-    public string ConnectionString { get; set; } = String.Empty;
-    public string DatabaseName { get; set; } = String.Empty;
-}
