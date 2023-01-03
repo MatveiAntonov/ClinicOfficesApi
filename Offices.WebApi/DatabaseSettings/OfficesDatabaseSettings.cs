e473060cebbaeddc9fd1864c6a69f2dbef95fa08
ProsTuha <tuhalf2002@mail.ru>
2023-01-03 14:31:05 +0300

    Added DatabaseSettings class and IDatabaseSettings interface to Offices.WebApi project

---
+++ Offices.WebApi/DatabaseSettings/OfficesDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Offices.WebApi.DatabaseSettings;
+
+public class OfficesDatabaseSettings : IOfficesDatabaseSettings
+{
+    public string CoursesCollectionName { get; set; } = String.Empty;
+    public string ConnectionString { get; set; } = String.Empty;
+    public string DatabaseName { get; set; } = String.Empty;
+}
