2a78a0d9477223f03ab086a1175739225181843b
ProsTuha <tuhalf2002@mail.ru>
2023-01-03 17:34:22 +0300

    Added database settings data

---
+++ Offices.Application/DatabaseSettings/IOfficesDatabaseSettings .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Offices.Application.DatabaseSettings;
+
+public interface IOfficesDatabaseSettings
+{
+    public string DatabaseCollectionName { get; set; }
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+}
---
+++ Offices.Application/DatabaseSettings/OfficesDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Offices.Application.DatabaseSettings;
+
+public class OfficesDatabaseSettings : IOfficesDatabaseSettings
+{
+    public string DatabaseCollectionName { get; set; } = String.Empty;
+    public string ConnectionString { get; set; } = String.Empty;
+    public string DatabaseName { get; set; } = String.Empty;
+}
