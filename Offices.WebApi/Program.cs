b97e67327ca62a3077e0201aaf916f7fd7425f6c
ProsTuha <tuhalf2002@mail.ru>
2023-01-04 17:26:47 +0300

    Added Cors + DI

--- Offices.WebApi/Program.cs
+++ Offices.WebApi/Program.cs
@@ -1,16 +1,44 @@
 using Microsoft.Extensions.Options;
 using Offices.Application.DatabaseSettings;
+using Offices.Application.Interfaces;
+using Offices.Application.Repositories;
+using Offices.WebApi.Mappings;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+
+services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
 
 services.Configure<OfficesDatabaseSettings>(
     builder.Configuration.GetSection(nameof(OfficesDatabaseSettings)));
 
 services.AddSingleton<IOfficesDatabaseSettings>(provider =>
         provider.GetRequiredService<IOptions<OfficesDatabaseSettings>>().Value);
-services.AddControllers();
+
+services.AddScoped<IOfficeRepository, OfficeRepository>();
+
+services.AddAutoMapper(typeof(MappingProfile));
+
 
 var app = builder.Build();
+
+app.UseRouting();
+
+app.UseCors("AllowAll");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
