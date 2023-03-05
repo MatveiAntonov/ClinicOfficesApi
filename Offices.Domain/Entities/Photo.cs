c7b6d36a4df02de4540173e88e0ad3e322e9d7c6
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 21:07:03 +0300

    Added photo entity

---
+++ Offices.Domain/Entities/Photo.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Offices.Domain.Entities
+{
+    public class Photo
+    {
+		[BsonId]
+		[BsonRepresentation(BsonType.Int32)]
+		public int Id { get; set; }
+
+		[BsonElement("PhotoUrl")]
+		public string PhotoUrl { get; set; } = String.Empty;
+
+		[BsonElement("PhotoName")]
+		public string PhotoName { get; set; } = String.Empty;
+    }
+}
